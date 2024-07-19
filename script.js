8e67e9d291292868ddc3698c7c3fbb4d41c1e435
CaxBuddy9 <robinbouvet@gmail.com>
2024-07-19 19:31:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function openModal(imgSrc, description) {
     var modal = document.getElementById('nftModal');
     var modalImg = document.getElementById('modalImg');
-    var captionText = document.getElementBy
+    var captionText = document.getElementById('caption');
+
+    modal.style.display = "block";
+    modalImg.src = imgSrc;
+    captionText.innerHTML = description;
+}
+
+function closeModal() {
+    var modal = document.getElementById('nftModal');
+    modal.style.display = "none";
+}
+
+// Fermer la modal lorsque l'utilisateur clique en dehors de l'image
+window.onclick = function(event) {
+    var modal = document.getElementById('nftModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
